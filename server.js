d49603aa62db0e39d872081e0f4a39f4cda6e0f0
Snigdha29 <snigdha.twilight@gmail.com>
2021-10-08 18:29:16 +0530

    Add dynamic GET route

--- server.js
+++ server.js
@@ -14,6 +14,15 @@ app.get('/users', function(req,res){
     })
 })
 
-app.listen(8888,function(){
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
+
+app.listen(8088,function(){
     console.log("server is running")
 })
